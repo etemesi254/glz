413faaf7231044f899f0b5890513aa18688c2f5e
caleb <etemesicaleb@gmail.com>
2023-06-07 23:39:17 +0300

    glz/compress: Define constants

---
+++ src/constants.rs
@@ -0,0 +1,16 @@
+pub const MIN_MATCH: usize = 3;
+pub const WINDOW_SIZE: usize = 5;
+pub const OFFSET_BIT: u8 = 6;
+pub const LITERAL_BITS: u8 = 0;
+pub const ML_BITS: u8 = 3;
+pub const COMPRESSION_LEVEL: usize = 6;
+pub const UNCOMPRESSED: usize = 32;
+pub const DEPTH_STRIDE: usize = 6;
+pub const HASH_CHAINS_MINIMAL_MATCH: usize = 4;
+pub const HASH_CHAINS_LOG: usize = 6;
+
+pub const SLOP_BYTES: usize = 1 << 16;
+// Memory size
+pub const MEM_SIZE: usize = 16 * (1 << 20);
+
+pub const TOKEN: usize = 7;
